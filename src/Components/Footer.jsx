aa7ba7c96048ea0fee55883a0ea9b59fd6fb0ff9
Akash-Raghuwanshi <akashraghuwanshi99@gmail.com>
2024-04-02 12:29:20 +0530

    added footer and updated css

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { FaGithubSquare } from "react-icons/fa";
+import { IoLogoLinkedin } from "react-icons/io5";
+
+const Footer = () => {
+  return (
+    <footer style={styles.footer}>
+         <a href='https://github.com/akashraghuwanshi'target='_blank' 
+         style={{fontSize:'2rem',margin:'.5rem',color:'black'}}>
+          <FaGithubSquare/>
+          </a>
+         <a href='https://www.linkedin.com/in/akashraghuwanshi/' target='_blank'
+          style={{fontSize:'2rem',color:'blue'}}>
+            <IoLogoLinkedin />
+            </a>
+         <p style={{fontSize:'1rem'}}>
+          &copy; 2024 Your Website. All rights reserved.
+          </p>
+    </footer>
+  )
+
+  
+}
+export default Footer
+
+//Inline CSS for footer
+const styles ={
+  footer:{
+    backgroundColor: 'hsl(283, 33%, 96%)',
+    color:'red',
+    textAlign: 'center',
+    position: 'fixed',
+    left: '0',
+    bottom: '0',
+    width: '100%',
+    height:'8f0px'
+  }
+}
